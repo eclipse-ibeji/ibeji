eee4e21db00ade9b8a089156240acdaf89a57648
Ash Beitz <8304894+ashbeitz@users.noreply.github.com>
2023-07-18 11:16:06 -0700

    Add seat massage demo

--- samples/command/provider/src/provider_impl.rs
+++ samples/command/provider/src/provider_impl.rs
@@ -12,7 +12,6 @@ use samples_protobuf_data_access::sample_grpc::v1::digital_twin_provider::{
     SubscribeRequest, SubscribeResponse, UnsubscribeRequest, UnsubscribeResponse,
 };
 use serde_derive::{Deserialize, Serialize};
-use serde_json;
 use tonic::{Request, Response, Status};
 
 /// The reponse payload is empty.
